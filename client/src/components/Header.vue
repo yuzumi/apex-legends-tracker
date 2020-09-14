32fc0ebae419ad3aaa426b18a6094fe9d3b730d1
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2020-09-14 14:30:29 +0300

    feat(client): add header component

---
+++ client/src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="header">
+    <img :src="logo" />
+  </header>
+</template>
+
+<script>
+import logo from '@/assets/images/logo.png';
+
+export default {
+  name: 'Header',
+  data: () => ({
+    logo,
+  }),
+};
+</script>
+
+<style scoped>
+img {
+  width: 300px;
+}
+
+@media(max-width: 700px) {
+  img {
+    display: block;
+    margin: auto;
+  }
+}
+</style>
