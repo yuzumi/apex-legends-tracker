4f80386a32107e68271f38db9ef6589b04200c86
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2020-09-14 14:30:49 +0300

    feat(client): add search form component

---
+++ client/src/components/SearchForm.vue
@@ -0,0 +1,53 @@
+<template>
+  <form class="search-form" @submit.prevent="handleSubmit">
+    <div class="form-group">
+      <label for="platform">
+        Platform
+      </label>
+      <select name="platform" id="platform" v-model="platform">
+        <option value="psn">Playstation</option>
+        <option value="xbl">Xbox</option>
+        <option value="Origin">Origin</option>
+      </select>
+    </div>
+    <div class="form-group">
+      <label for="gamertag">
+        Gamertag
+      </label>
+      <input type="text" name="gamertag" id="gamertag" v-model.trim="gamertag" />
+    </div>
+    <div class="form-group">
+      <input type="submit" value="Submit" class="btn btn-submit">
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'SearchForm',
+  data: () => ({
+    platform: 'psn',
+    gamertag: '',
+  }),
+  methods: {
+    handleSubmit() {
+      const { platform, gamertag } = this;
+
+      if (!gamertag) {
+        this.$toasted.show('Please enter a gamertag');
+      } else {
+        this.$router.push({
+          name: 'profile',
+          params: {
+            platform,
+            gamertag,
+          },
+        });
+      }
+    },
+  },
+  beforeCreate() {
+    document.body.classList.add('body-bg-image');
+  },
+};
+</script>
