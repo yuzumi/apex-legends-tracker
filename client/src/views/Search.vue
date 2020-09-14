aab6dbb37e9cfc43e9020ef18b8fa15cf4818112
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2020-09-14 14:31:31 +0300

    feat(client): add search page

---
+++ client/src/views/Search.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="search">
+    <h1>Track Player Stats</h1>
+    <SearchForm />
+  </section>
+</template>
+
+<script>
+import SearchForm from '@/components/SearchForm.vue';
+
+export default {
+  name: 'Search',
+  components: {
+    SearchForm,
+  },
+};
+</script>
