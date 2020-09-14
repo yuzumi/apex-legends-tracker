1ad53e62ed181085a31f9b7553464f0d07a7d735
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2020-09-14 14:30:01 +0300

    chore(client.plugins): register vue-toasted

--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue';
+import VueToasted from 'vue-toasted';
+
 import App from '@/App.vue';
 import router from '@/router';
 import store from '@/store';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueToasted, {
+  duration: 3000,
+});
 
 new Vue({
   router,
