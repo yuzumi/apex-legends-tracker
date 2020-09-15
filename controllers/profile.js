eab69bd8eca1659dd440002b1d693585bb0d4664
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2020-09-15 14:58:07 +0300

    refactor(profile): add controller

---
+++ controllers/profile.js
@@ -0,0 +1,29 @@
+const fetch = require('node-fetch');
+
+const { TRACKER_API_KEY, TRACKER_API_URL } = require('../config');
+
+class ProfileController {
+  static async index(req, res) {
+    const headers = { 'TRN-Api-Key': TRACKER_API_KEY };
+  
+    const { platform, gamertag } = req.params;
+  
+    const url = `${TRACKER_API_URL}/profile/${platform}/${gamertag}`;
+  
+    try {
+      const response = await fetch(url, { headers });
+      const data = await response.json();
+  
+      data.errors
+        ? res.status(404).json({ message: 'Profile not found' })
+        : res.json(data);
+        
+    } catch (error) {
+      console.error(error);
+  
+      res.status(500).json({ message: 'Server Error' });
+    }
+  }
+}
+
+module.exports = ProfileController;
--- routes/profile.js
+++ routes/profile.js
@@ -1,30 +1,8 @@
 const { Router } = require('express');
-const fetch = require('node-fetch');
-
-const { TRACKER_API_KEY, TRACKER_API_URL } = require('../config');
+const ProfileController = require('../controllers/profile');
 
 const router = Router();
 
-router.get('/:platform/:gamertag', async (req, res) => {
-  const headers = { 'TRN-Api-Key': TRACKER_API_KEY };
-
-  const { platform, gamertag } = req.params;
-
-  const url = `${TRACKER_API_URL}/profile/${platform}/${gamertag}`;
-
-  try {
-    const response = await fetch(url, { headers });
-    const data = await response.json();
-
-    data.errors
-      ? res.status(404).json({ message: 'Profile not found' })
-      : res.json(data);
-      
-  } catch (error) {
-    console.error(error);
-
-    res.status(500).json({ message: 'Server Error' });
-  }
-});
+router.get('/:platform/:gamertag', ProfileController.index);
 
 module.exports = router;
